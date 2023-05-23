a8d2e2f29436a9b8e25ed376fd94586532909828
Animesh Maiti <a.maiti770@gmail.com>
2023-05-23 21:08:40 +0530

    variables

--- variable.js
+++ variable.js
@@ -1,1 +1,28 @@
-console.log("Hello World")
+var a = 345;//var is used to declare a variable but it
+            // is var can be redeclared and reinitialized it could be a problem
+            // so we use let and const mostly
+var a=67
+{
+    var a = "Hello World"; 
+    console.log(a);
+}
+console.log(a);
+// let is used to declare a variable but it can be reinitialized but not redeclared
+let b = 45;
+//let b=56; // it will give an error
+b=45;//reinitialization is possible
+console.log(b);
+{// in block scope it will not give an error
+    let b = "Hello World";// this b is different from the above b
+    console.log(b);
+}
+console.log(b);
+// const is used to declare a variable but it can neither be reinitialized nor redeclared
+const c=45;
+//c=45; // it will give an error
+//const c=45; // it will give an error
+let d=undefined;
+console.log(d);
+let e=null;
+console.log(e);
+
