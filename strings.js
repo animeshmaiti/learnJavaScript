e9d574a9486a3d57d60e855ba90fb83df05b2bee
Animesh Maiti <a.maiti770@gmail.com>
2023-06-02 11:22:28 +0530

    update

--- strings.js
+++ strings.js
@@ -4,3 +4,8 @@ console.log(name2,name1);//string can declare in single quote and double quote
 // template literals
 let sentence = `${name1} is following ${name2}`//`` this is called back ticks
 console.log(sentence);
+// escape sequence character
+console.log("hello\" how are you")
+console.log("hello\nhow are you")//new line 
+console.log("hello\thow are you")//tab
+console.log("hello\rhow are you")//carriage return 
