6db3c65ef62966dacbd66d443c3f15f6ae2114c3
Animesh Maiti <a.maiti770@gmail.com>
2023-06-06 20:53:57 +0530

    map filter reduce

---
+++ map_filter_reduce.js
@@ -0,0 +1,49 @@
+// map not effecting original array
+/*forEach: This iterates over a list and applies some operation
+with side effects to each list member (example: saving every list item to the database)
+and does not return anything.*/
+
+/*map: This iterates over a list, transforms each member of that
+list, and returns another list of the same size with the transformed
+members (example: transforming list of strings to uppercase). It does
+not mutate the array on which it is called (although the callback function may do so).*/
+let array=[65,78,67,79]
+let a=array.map((element,index,arr)=>{
+    console.log(element,index,arr);
+    /*
+    65 0 [ 65, 78, 67, 79 ]
+    78 1 [ 65, 78, 67, 79 ]
+    67 2 [ 65, 78, 67, 79 ]
+    79 3 [ 65, 78, 67, 79 ]
+    */
+    return element+1;
+})
+console.log(a);//[ 66, 79, 68, 80 ]
+
+// array filter method not effecting original array
+let arr2=[65,78,67,79,56,889,89]
+let b=arr2.filter((value)=>{
+    return value>100;
+})
+console.log(b);//[ 889 ]
+
+//Array reduce method
+// syntax: array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+//Reducer function parameters:
+// total	Required.
+//          The initialValue, or the previously returned value of the function.
+
+// currentValue	    Required.
+//                  The value of the current element.
+
+// currentIndex	    Optional.
+//                  The index of the current element.
+
+// arr	    Optional.
+//          The array the current element belongs to.
+let arr3=[1,2,3,4,5,6,7,8,9];
+let newArr=arr3.reduce((total,currentValue)=>{
+    console.log(total+'+'+currentValue);
+    return total+currentValue;
+})
+console.log(newArr);
