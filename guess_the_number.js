b6bf1e776d01132fc637447cd4659aed95eea33e
Animesh Maiti <a.maiti770@gmail.com>
2023-06-07 09:59:05 +0530

    update

--- guess_the_number.js
+++ guess_the_number.js
@@ -1,1 +1,17 @@
-let num=78;
+const prompt = require("prompt-sync")({ sigint: true });
+let num=Math.floor(Math.random() * 101);
+// console.log(num);
+let inputVal=null,points=100,numberOfGuess=0;
+while (num!=inputVal) {
+    numberOfGuess++
+    inputVal=prompt('enter your guess: ');
+    parseInt(inputVal);// prompt taking input as string so typecast into a int
+    if(inputVal>num){
+        console.log('your guessed number is higher then the original number');
+        points--
+    } else if(inputVal<num){
+        console.log('your guessed number is lower then the original number');
+        points--
+    }
+}
+console.log(`you guessed ${inputVal} it right in ${numberOfGuess} guess and your point is ${points}`);
