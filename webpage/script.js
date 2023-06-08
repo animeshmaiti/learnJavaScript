9aa07a4a69dd9c25ffaf7a8324b824e5c437b8d0
Animesh Maiti <a.maiti770@gmail.com>
2023-06-08 10:27:52 +0530

    update

--- webpage/script.js
+++ webpage/script.js
@@ -1,1 +1,18 @@
-console.log(console)
+// console.log(console)
+let runAgain = true;
+const canDrive = (age) => {
+    return age >= 18
+}
+while (runAgain) {
+    let age = prompt('Enter your age');
+    age = Number.parseInt(age);
+    if(age<0){
+        console.error('invalid age input')
+        break;
+    }
+    if (canDrive(age))
+        alert('you can drive');
+    else
+        alert('you cannot drive');
+    runAgain = confirm("do you want the prompt again");// confirm is a type of alert if you cancel it return false 
+}
---
+++ webpage/script2.js
@@ -0,0 +1,7 @@
+// let num=prompt('enter a number');
+// num=Number.parseInt(num);
+// if(num>7){
+//     location.href='https://google.com';
+// }
+let color=prompt('enter a color');
+document.body.style.background=color;
