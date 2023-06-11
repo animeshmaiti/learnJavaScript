bfb1e73702cf4166752f96f7f3ed4aa0ee4cd7cc
Animesh Maiti <a.maiti770@gmail.com>
2023-06-11 09:37:46 +0530

    map filter reduce

--- map_filter_reduce.js
+++ map_filter_reduce.js
@@ -7,6 +7,24 @@ and does not return anything.*/
 list, and returns another list of the same size with the transformed
 members (example: transforming list of strings to uppercase). It does
 not mutate the array on which it is called (although the callback function may do so).*/
+// syntax array.map(function(currentValue, index, arr), thisValue)
+/* https://www.w3schools.com/jsref/jsref_map.asp
+function()	Required.
+            A function to be run for each array element.
+
+currentValue	Required.
+                The value of the current element.
+
+index	Optional.
+        The index of the current element.
+
+arr	Optional.
+    The array of the current element.
+
+thisValue	Optional.
+            Default value undefined.
+            A value passed to the function to be used as its this value.
+*/
 let array=[65,78,67,79]
 let a=array.map((element,index,arr)=>{
     console.log(element,index,arr);
