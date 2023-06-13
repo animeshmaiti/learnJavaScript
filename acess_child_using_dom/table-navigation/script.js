f73806c03031e761dd0473e34b2383c2730541f4
Animesh Maiti <a.maiti770@gmail.com>
2023-06-13 11:07:17 +0530

    update

---
+++ acess_child_using_dom/table-navigation/script.js
@@ -0,0 +1,9 @@
+// https://www.w3schools.com/jsref/dom_obj_table.asp
+let t=document.body.firstElementChild.firstElementChild;
+console.log(t);
+console.log(t.rows);
+console.log(t.caption);
+console.log(t.tHead);
+console.log(t.tFoot);
+console.log(t.tBodies);
+console.log(t.rows[2].rowIndex);
