2069bdb215064c73c9de3ebefed2ea4dce4bf2cc
Animesh Maiti <a.maiti770@gmail.com>
2023-06-14 09:48:00 +0530

    different type of dom search

--- acess_child_using_dom/searching-dom/script.js
+++ acess_child_using_dom/searching-dom/script.js
@@ -8,3 +8,11 @@ let title=document.getElementsByClassName('card-title')[0];
  //using id
 let text=document.getElementById('c-text');
 text.style.color='blue';
+let cTitles=document.querySelectorAll('.card-title');
+cTitles[0].style.color='red';
+cTitles[1].style.color='green';
+cTitles[2].style.color='blue';
+document.querySelector('.this').style.color='cyan';
+console.log(document.getElementsByTagName('a'));
+console.log(document.querySelector('.card').getElementsByTagName('a'));
+console.log(document.getElementsByName('search'));
