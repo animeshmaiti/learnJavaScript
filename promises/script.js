891836b70bd457a3d52315f0d31bdf9ddcd0400f
Animesh Maiti <a.maiti770@gmail.com>
2023-06-29 09:26:11 +0530

    update

--- promises/script.js
+++ promises/script.js
@@ -8,15 +8,19 @@ let p1 = new Promise((resolve, rejected) => {
 let p2 = new Promise((resolve, rejected) => {
     console.log("Promise 2 pending");
     setTimeout(() => {
+        // this is a manual error not an actual error
+        // if you write "i am success" in rejected it will be considered
+        // as a success but we are writing an error so it will be considered as an error
         rejected(new Error("i am an error"));
     }, 2000)
 })
 // To get the value
+// .then((value) => {}) .then will be called when the promise is resolved
 p1
-    .then((value) => {
+    .then((value) => {// this value is the value that we passed in resolve
         console.log(value)
     })
-    .catch((err) => {
+    .catch((err) => {// this err is the value that comes in error in in rejected
         console.log(err)
     })
 
