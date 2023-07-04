96df97d43d74e3329c82af8d6fc567f7af925ece
Animesh Maiti <a.maiti770@gmail.com>
2023-07-04 13:22:24 +0530

    add

---
+++ async-await-ex/script2.js
@@ -0,0 +1,20 @@
+let data = ['fetching data from connected device', 'user found name->"rahul17"', 'getting all data from chrome data folder', 'fetching user password', 'user password found,stored in config.xml', 'history,cookies,cache,bookmarks found', 'fetching', 'fetched and stored in data.xml file']
+let Data = [];
+let out = [];
+const fetchData = async () => {
+    for (let i = 0; i < data.length; i++) {
+        Data[i] = new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(data[i]);
+            }, i * 1000);
+        })
+    }
+    for (let i = 0; i < Data.length; i++) {
+        out[i] = await Data[i];
+        console.log(out[i]);
+    }
+    return 'successfully hacked';
+};
+fetchData().then((data) => {
+    console.log(data);
+});
