1a6ed42cc1b94d6b9ef4e7eaa4313241a90990c6
Animesh Maiti <a.maiti770@gmail.com>
2023-10-24 21:28:16 +0530

    update file structure

--- pdf_merger/main.js
+++ pdf_merger/main.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: './public/data/uploads/' })
 const { mergePdfs } = require('./merger')
 const app = express()
 const port = 3000
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 
 app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
     const d=await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
-    res.redirect(`http://localhost:3000/static/merged/${d}.pdf`)
+    res.redirect(`http://localhost:3000/static/data/merged/${d}.pdf`)
 
     // console.log(req.files)
     // res.send({ data: req.files })
